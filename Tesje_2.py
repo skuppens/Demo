4c22b489e01b0c2d8747545e87f76e15724eba8a
Stijn Kuppens <Stijn.Kuppens@kdg.be>
2020-12-13 15:06:48 +0100

    Test Add to new branch

---
+++ Tesje_2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# Features
+# Python program to print the ASCII Value of some Character
+# In the variable 'c' we can assign different
+# characters of which we want the ASCII value shown
+#
+
+# Generic libs
+# import os
+# import sys
+
+# Other Libs
+
+
+# Own libs
+
+
+# Common info
+__author__ = "Stijn Kuppens"
+__copyright__ = "Hahaha"
+__credits__ = "Stijn Kuppens"
+__version__ = "0.1"
+__maintainer__ = "Stijn Kuppens"
+__email__ = "stijn.kuppens@kdg.be"
+__status__ = "DEV"
+
+# {code here}
